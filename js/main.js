33adb107c509d7510a99d3005d5a3316087b72f5
goodkp21 <karegoodman@yahoo.com>
2016-11-10 00:07:11 -0500

    newCommit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
+$ (document).ready(function(){
+
+var requestURL = "https://goodkp21.github.io/my-portfolio/data.json";
+  
+  $.ajax({
+    method: "GET",
+    url: requestURL,
+    dataType: "json",
+    success: mySuccessListener,
+    error: myErrorListener
+  });
+
+
+  
+});
+
+function mySuccessListener(data)
+{
+	// var projectName = getParameterByName('project-id'); // null (absent)
+	// var nextProjectName = ""
+	var listNames = [];
+	var listThumbnails = [];
+	for(i = 0; i<data.length; i++){
+		var currentObj = data[i];
+		var projectName = currentObj.name
+		var thumbs = currentObj.thumbnail
+		listThumbnails = listThumbnails + thumbs;
+		listNames = listNames + projectName;
+	}
+
+
+	$("#project1").css("background-image", "url(" + listThumbnails[0] + ")");
+	$("#project1").css("content", listNames[0]);
+
+
+	//alert(listThumbnails);
+}
+
+
+
+
+function myErrorListener(xhr,ajaxOptions,thrownError)
+{
+  alert(thrownError);
+}
+
 
